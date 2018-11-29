23ea9a9b86ac42ece22a89cfe00c21bf6859f939
Peter Chu <petachew@uw.edu>
2018-11-28 16:53:43 -0800

    Update pcmatrix.h

--- chu_peter/pcMatrix/pcmatrix.h
+++ chu_peter/pcMatrix/pcmatrix.h
@@ -1,11 +1,5 @@
 /*
  *  matrix task processor pcmatrix header file
- *  Based on Operating Systems: Three Easy Pieces by R. Arpaci-Dusseau and A. Arpaci-Dusseau
- * 
- *  Wes J. Lloyd
- *  University of Washington, Tacoma
- *  TCSS 422 - Operating Systems
- *  Winter 2018
  */
 
 #define OUTPUT 1
